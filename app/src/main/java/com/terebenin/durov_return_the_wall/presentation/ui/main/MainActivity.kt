0c0e542d498d5e99d5453c8507e952835c493505
Evgeny Terebenin <terebenin.e.p@gmail.com>
2020-05-08 17:53:19 +0300

    updated gradle dependencies

--- app/src/main/java/com/terebenin/durov_return_the_wall/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/terebenin/durov_return_the_wall/presentation/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.terebenin.durov_return_the_wall.presentation.ui.main
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.terebenin.durov_return_the_wall.BuildConfig
 import com.terebenin.durov_return_the_wall.R
 import com.terebenin.durov_return_the_wall.presentation.ui.global.VkApplication.Companion.prefs
 
@@ -20,7 +21,10 @@ class MainActivity : AppCompatActivity() {
                 .commitNow()
         }
 
-        Toast.makeText(this, prefs.accessToken.token, Toast.LENGTH_SHORT).show()
+        //FIXME удалить код перед релизом
+        if (BuildConfig.DEBUG) {
+            Toast.makeText(this, prefs.accessToken.token, Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
