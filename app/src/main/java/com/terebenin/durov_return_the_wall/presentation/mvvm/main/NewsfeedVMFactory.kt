018954c56fa2c6525ed94b1746db12798bb9226c
Evgeny Terebenin <terebenin.e.p@gmail.com>
2020-05-08 18:32:02 +0300

    fix viewmodel factory create

--- app/src/main/java/com/terebenin/durov_return_the_wall/presentation/mvvm/main/NewsfeedVMFactory.kt
+++ app/src/main/java/com/terebenin/durov_return_the_wall/presentation/mvvm/main/NewsfeedVMFactory.kt
@@ -4,8 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.terebenin.durov_return_the_wall.domain.newsfeed.NewsfeedInteractor
 
-class NewsfeedVMFactory(private val interactor: NewsfeedInteractor) : ViewModelProvider.Factory {
+class NewsfeedVMFactory(private val interactor: NewsfeedInteractor) :
+    ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return NewsfeedVMFactory(interactor).create(NewsfeedViewModel::class.java) as T
+        return NewsfeedViewModel(interactor) as T
     }
 }
